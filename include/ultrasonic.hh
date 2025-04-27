ac4dc97fd1463c47d099217f3aef4cf56e83420d
amateurstock <immanuellefyan@gmail.com>
2025-04-27 16:02:51 +0800

    Finally started working on the ultrasonic logic. this will take a while. gonna commit for now, need to work on the slave arduino logic for the commands esp32 is sending.

--- include/ultrasonic.hh
+++ include/ultrasonic.hh
@@ -1,6 +1,59 @@
 #ifndef ULTRASONIC_HH
 #define ULTRASONIC_HH
 
+#include <Arduino.h>
 
+class Ultrasonic {
+private:
+    uint8_t _trig_pin;
+    uint8_t _echo_pin;
+    uint32_t _max_distance = 100;
+
+public:
+    Ultrasonic(
+        uint8_t trig_pin,
+        uint8_t echo_pin,
+        uint32_t max_distance
+    );
+
+    Ultrasonic(
+        uint8_t trig_pin,
+        uint8_t echo_pin
+    );
+
+    uint32_t ping_distance_cm();
+};
+
+class UltrasonicArray {
+public:
+    typedef enum {
+        SONAR1 = 1,
+        SONAR2,
+        SONAR3
+    }SonarSelect;
+
+    UltrasonicArray(
+        Ultrasonic sonar1,
+        Ultrasonic sonar2,
+        Ultrasonic sonar3
+    );
+    void ping_distances_cm();
+    uint32_t return_distance(SonarSelect sonar);
+
+private:
+    Ultrasonic _sonar1;
+    Ultrasonic _sonar2;
+    Ultrasonic _sonar3;
+    
+    SonarSelect _select = SONAR1;
+
+    // Just make it max so that the outside logic won't take "0" as a
+    // signal to start doing something
+    uint32_t _sonar1_out = UINT32_MAX;
+    uint32_t _sonar2_out = UINT32_MAX;
+    uint32_t _sonar3_out = UINT32_MAX;
+
+
+};
 
 #endif
--- src/ultrasonic.cc
+++ src/ultrasonic.cc
@@ -1,3 +1,65 @@
 #include "ultrasonic.hh"
 
+// Individual sonars shit
+Ultrasonic::Ultrasonic(
+    uint8_t trig_pin,
+    uint8_t echo_pin,
+    uint32_t max_distance
+) {
+    _trig_pin = trig_pin;
+    _echo_pin = echo_pin;
+    _max_distance = max_distance;
 
+    pinMode(_trig_pin, OUTPUT);
+    pinMode(_echo_pin, INPUT);
+
+    digitalWrite(_trig_pin, LOW);
+}
+
+Ultrasonic::Ultrasonic(
+    uint8_t trig_pin,
+    uint8_t echo_pin
+) {
+    _trig_pin = trig_pin;
+    _echo_pin = echo_pin;
+
+    pinMode(_trig_pin, OUTPUT);
+    pinMode(_echo_pin, INPUT);
+}
+
+uint32_t Ultrasonic::ping_distance_cm() {
+
+}
+
+
+
+
+// Arrays shit
+void UltrasonicArray::ping_distances_cm() {
+    switch (_select) {
+        case SONAR1: {
+
+            break;
+        }
+        case SONAR2: {
+
+            break;
+        }
+        case SONAR3: {
+
+            break;
+        }
+    }
+}
+
+uint32_t UltrasonicArray::return_distance(SonarSelect sonar) {
+    uint32_t ret = 0;
+    switch (sonar) {
+        case SONAR1: ret = _sonar1_out; break;
+        case SONAR2: ret = _sonar2_out; break;
+        case SONAR3: ret = _sonar3_out; break;
+        default: break;
+    }
+
+    return ret;
+}
