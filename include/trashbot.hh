7c27def14396cddd7cf6b1bb1129a72ea92609b5
amateurstock <immanuellefyan@gmail.com>
2025-04-29 17:27:37 +0800

    Just some boilerplate code for now.

--- include/trashbot.hh
+++ include/trashbot.hh
@@ -1,6 +1,14 @@
 #ifndef TRASHBOT_HH
 #define TRASHBOT_HH
 
+#include "main.hh"
+
 #include <Arduino.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+void check_trash_obstacle(void *params);
+void trash_colletion(void);
+void obstacle_avoidance(void);
 
 #endif
--- src/trashbot.cc
+++ src/trashbot.cc
@@ -1,3 +1,33 @@
 #include "trashbot.hh"
 
+const char *check_trash_obstacle_tag = "check_trash_obstacle";
+extern sensors_t sensors_state;
 
+constexpr uint8_t TRASH_THRESHOLD = 50;
+constexpr uint8_t OBSTACLE_THRESHOLD = 100;
+
+void check_trash_obstacle(void *params) {
+    user_logger(check_trash_obstacle_tag, "Waiting for the rest to init.");
+    vTaskDelay(pdMS_TO_TICKS(2000));
+    for (;;) {
+        if (sensors_state.distance_1 <= TRASH_THRESHOLD ||
+            sensors_state.distance_2 <= TRASH_THRESHOLD) {
+            trash_colletion();
+            continue;
+        }
+        vPortYield();
+
+        if (sensors_state.distance_3 <= OBSTACLE_THRESHOLD) {
+            obstacle_avoidance();
+        }
+        vPortYield();
+
+        vTaskDelay(10);
+    }
+}
+
+void trash_colletion(void) {
+}
+
+void obstacle_avoidance(void) {
+}
